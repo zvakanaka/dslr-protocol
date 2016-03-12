c9ee83ee90e906f9e3845620c5666dce0ab75adb
zvakanaka <8365885+zvakanaka@users.noreply>
2016-03-12 01:50:48 +0000

    Server works with one cam and one ctr with cap command

--- dslr-server.py
+++ dslr-server.py
@@ -9,6 +9,7 @@
 #############################################################################
 import socket
 import sys #command line args
+import os
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 5555              # Default port if none specified in args
@@ -24,8 +25,9 @@ try:
     s.bind((HOST, PORT))
 except socket.error as e:
     print 'ERROR: Failed to bind on port ', PORT
-    
-print 'listening on port ', PORT
+    #os.system("fuser 5555/tcp")
+    #s.bind((HOST, PORT))
+print 'Listening on port ', PORT
 s.listen(1)
 
 while 1:
@@ -34,12 +36,28 @@ while 1:
     data = conn.recv(1024)
     print data
     
-    if not data: break
+    conn2, addr2 = s.accept()
+    print 'Connected by', addr
+    data2 = conn2.recv(1024)
+    print data2
+    
+    if not data or not data2: break
+    print "Datas: ", data
+    print "Datas2: ", data2
     #read request line
-    #requestLine = data.partition("\n")[0]
+    command = data.partition(" ")[0]
+    command2 = data2.partition(" ")[0]
+    if command == "cam":
+        ctrEvent = data2.partition(" ")[2]
+        print "PArtition ", ctrEvent, " Whole ", data2
+        conn.sendall(ctrEvent)
+    elif command2 == "cam":
+        ctrEvent = data.partition(" ")[2]
+        print "PArtition ", ctrEvent, " Whole ", data
+        conn2.sendall(ctrEvent)
     #requestMethod = requestLine.split(' ')[0]
     #requestObject = requestLine.split(' ')[1]
     #httpVersion = requestLine.split(' ')[2]
     
-    conn.sendall('data here')
+    #conn.sendall('data here')
     conn.close()
