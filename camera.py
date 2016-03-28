b28ca3fbc65d7b9f6fda7c350d19b013ada3ed26
zvakanaka <8365885+zvakanaka@users.noreply>
2016-03-28 10:53:31 -0600

    Camera client now set for Linux webcam

--- camera.py
+++ camera.py
@@ -3,13 +3,17 @@ import socket
 
 HOST = 'howtoterminal.com'
 PORT = 5555
+BUFSIZE = 4096
+#variables are loaded according to camera type
+#arguments are parsed and added before execution
 
 while (1):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
+    print 'Connection:', s
     s.sendall('cam')# I am a camera
-
-    data = s.recv(1024)
+     
+    data = s.recv(BUFSIZE)
     print 'Received: ', data
 
     action = data.split(' ')[0]
@@ -17,13 +21,18 @@ while (1):
         value = data.split(' ')[1]
     else:
         value = ''
-        
+
     if action == "cap":
-        os.system("raspistill --width 200 --height 150 -v --nopreview -o picture.jpg")
-        os.#system("scp pic.jpg pi@howtoterminal.com:~/public_html/")
+        os.system("uvccapture -m -x120 -B50 -q30 -opic.jpg")
+        #upload picture
+        upFile = "pic.jpg"
+        #TODO: get huge files working
+        bytes = open(upFile).read()
+        print len(bytes)
+        s.send(bytes)
+        #os.system("scp pic.jpg pi@howtoterminal.com:~/public_html/dslr-protocol/pic.jpg")
     elif action == "iso":
         os.system("raspistill --width 200 --height 150 -v --nopreview -o pic.jpg " + value)
-
     
    # if action == "cap":
    #     os.system("gphoto2 --capture-image")
