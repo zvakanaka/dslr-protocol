b3a7a2df64aa51113171d8fe19b5e5bcedb46160
zvakanaka <8365885+zvakanaka@users.noreply>
2016-04-01 12:00:03 -0600

    Add config modules

---
+++ picam.py
@@ -0,0 +1,15 @@
+#webcam command settings module
+bin = '/usr/bin/raspistill --nopreview '
+#raspistill --width 200 --height 150 -v --nopreview -o pic.jpg
+capture = ''
+filename = ' -o '
+#get = ' --get-config='
+
+#set = ' --set-config='
+
+#quality = '/main/capturesettings/imagequality'
+#basic = '0'
+
+#fstop = '/main/capturesettings/f-number'
+#iso = '/main/imgsettings/iso'
+#shutterspeed = '/main/capturesettings/shutterspeed'
---
+++ webcam.py
@@ -0,0 +1,15 @@
+#webcam command settings module
+bin = '/usr/bin/uvccapture '
+#uvccapture -m -x120 -B50 -q30 -opic.jpg
+capture = ' -m'
+filename = ' -o'
+# get = ' --get-config='
+
+# set = ' --set-config='
+
+# quality = '/main/capturesettings/imagequality'
+# basic = '0'
+
+# fstop = '/main/capturesettings/f-number'
+# iso = '/main/imgsettings/iso'
+# shutterspeed = '/main/capturesettings/shutterspeed'
