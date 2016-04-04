be154f9a4958f35a6ab2c1bc6b3e58df6bf86e54
zvakanaka <8365885+zvakanaka@users.noreply>
2016-04-04 16:35:30 -0600

    Add arg options

--- controller.py
+++ controller.py
@@ -3,11 +3,18 @@ import socket
 HOST = 'localhost'
 PORT = 5555
 BUFSIZE = 4096
+
+if len(sys.argv) >= 2:
+    PORT = int(sys.argv[1])
+
+if len(sys.argv) >= 3:
+    HOST = sys.argv[2]
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'PORT: ', PORT, ', HOST: ', HOST
 
 def sendCommand(command):
 	s.connect((HOST, PORT))
-	#s.sendall('ctr cap')
 	s.sendall(command)
 #	result = s.recv(BUFSIZE)
 #	print 'RESULT: ', result
