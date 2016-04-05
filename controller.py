15edf907fa5a4bd68c37026e4520d83f23635e5a
zvakanaka <8365885+zvakanaka@users.noreply>
2016-04-05 12:11:06 -0600

    Add hostname and port options to sendCommand

--- controller.py
+++ controller.py
@@ -1,25 +1,13 @@
 import socket
 import sys
 
-HOST = 'localhost'
+HOST = 'howtoterminal.com'
 PORT = 5555
 BUFSIZE = 4096
 
-if len(sys.argv) >= 2:
-    PORT = int(sys.argv[1])
-
-if len(sys.argv) >= 3:
-    HOST = sys.argv[2]
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'PORT: ', PORT, ', HOST: ', HOST
-
-def sendCommand(command):
-	s.connect((HOST, PORT))
-	s.sendall(command)
-#	result = s.recv(BUFSIZE)
-#	print 'RESULT: ', result
-	s.close()
-
-def what():
-  print "yo"
+def sendCommand(command, HOST, PORT):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print 'PORT: ', PORT, ', HOST: ', HOST
+    s.connect((HOST, PORT))
+    s.sendall(command)
+    s.close()
